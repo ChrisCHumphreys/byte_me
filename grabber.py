a55ebfdaca76c823b9035361500ff86d129e8ce2
root <root@localhost.localdomain>
2019-09-14 13:19:10 -0500

    switching to inputting first line as hex

--- grabber.py
+++ grabber.py
@@ -5,25 +5,28 @@ import time
 import re
 import sys
 
-# items = ['ab', 'aa', 'aaa', 'aaaaa']
+size = 5
+items = ['ab', 'aa', 'aaa', 'aaaaa']
+items[0] = (1024).to_bytes(2, byteorder='big')
+print(items[0])
 
 def clean_output(query, response):
     spaces = int(-1 * (len(query) - 10))
     pattern = r'[0-9*a-f*0-9*]{34,}'
     code = re.findall(pattern, str(response))
     try:
-        print(str(query + (spaces * ' ') + " : " + code[0] + '\n'))
+        print(str((spaces * ' ') + " : " + code[0] + '\n'))
         return str(query + (spaces * ' ') + " : " + code[0] + '\n')
     except: 
         return str(query + " : error\n")
 
 outfile = open("results.txt", "w")
-infile = open(sys.argv[1], "r")
-
-items = infile.read()
-items = items.split()
-
-infile.close()
+# infile = open(sys.argv[1], "r")
+# 
+# items = infile.read()
+# items = items.split()
+# 
+# infile.close()
 
 HOST = 'crypto.chal.csaw.io'    # The remote host
 PORT = 1003              # The same port as used by the server
@@ -34,13 +37,19 @@ data = s.recv(1024)
 outfile.write(clean_output('', data))
 time.sleep(1)
 
-for item in items:
-    s.sendall(str(item + '\n').encode('utf-8'))
-    time.sleep(1)
-    data = s.recv(1024)
-    # print(str(data))
-    outfile.write(clean_output(item, data))
+s.sendall((1).to_bytes(1, byteorder='big'))
+s.sendall(b'\n')
+time.sleep(1)
+data = s.recv(1024)
+print(repr(data))
+s.sendall((0).to_bytes(1, byteorder='big'))
+s.sendall(b'\n')
+time.sleep(1)
+data = s.recv(1024)
+# print(str(data))
+# outfile.write(clean_output(item, data))
+print(repr(data))
 
 s.close()
-outfile.close()
+#outfile.close()
 
